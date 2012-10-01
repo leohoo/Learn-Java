a05fdd29e3f405c2888629e49b56d71698778928
Wei Liu <leohoo@hotmail.com>
2012-10-01 19:06:38 +0900

    first commit

---
+++ DataTypesAndOperators/DataTypesAndOperators/src/Identifiers.java
@@ -0,0 +1,19 @@
+
+public class Identifiers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int _ = 2;
+		int $2 = 3;
+		int ανδρος = 3;
+		int α = 2;
+		
+		String  汉字="Chinese";
+		
+		System.out.println("ανδρος+α=" + (ανδρος+α));
+		System.out.println(汉字);
+	}
+
+}
