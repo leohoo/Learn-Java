3f0d38f15106f468dd853961f80671426598f0e0
Wei Liu <leohoo@hotmail.com>
2012-10-16 11:12:35 +0900

    private

--- Classes/src/nestedClass/OuterClass.java
+++ Classes/src/nestedClass/OuterClass.java
@@ -10,7 +10,7 @@ import access_test.AccessTest;
 public class OuterClass {
 
 	class InnerClass{
-		void f(){
+		private void f(){
 			System.out.println(AccessTest.getMethodName());			
 		}
 		
@@ -46,6 +46,7 @@ public class OuterClass {
 		
 		InnerClass a = t.new InnerClass();
 		a.bar();
+		a.f();
 		
 		StaticInnerClass s = new StaticInnerClass();
 	}
