5754564649932a53d5c9c4ea859353f06413cf44
Wei Liu <leohoo@hotmail.com>
2012-10-23 10:12:01 +0900

    design patterns

---
+++ DesignPatterns/src/momento/MomentoSample.java
@@ -0,0 +1,53 @@
+package momento;
+
+import java.util.List;
+import java.util.ArrayList;
+class Originator {
+    private String state;
+    // The class could also contain additional data that is not part of the
+    // state saved in the memento.
+ 
+    public void set(String state) {
+        System.out.println("Originator: Setting state to " + state);
+        this.state = state;
+    }
+ 
+    public Memento saveToMemento() {
+        System.out.println("Originator: Saving to Memento.");
+        return new Memento(state);
+    }
+ 
+    public void restoreFromMemento(Memento memento) {
+        state = memento.getSavedState();
+        System.out.println("Originator: State after restoring from Memento: " + state);
+    }
+ 
+    public static class Memento {
+        private final String state;
+ 
+        public Memento(String stateToSave) {
+            state = stateToSave;
+        }
+ 
+        public String getSavedState() {
+            return state;
+        }
+    }
+}
+ 
+class MomentoSample {
+    public static void main(String[] args) {
+        List<Originator.Memento> savedStates = new ArrayList<Originator.Memento>();
+ 
+        Originator originator = new Originator();
+        originator.set("State1");
+        originator.set("State2");
+        savedStates.add(originator.saveToMemento());
+        originator.set("State3");
+        // We can request multiple mementos, and choose which one to roll back to.
+        savedStates.add(originator.saveToMemento());
+        originator.set("State4");
+ 
+        originator.restoreFromMemento(savedStates.get(1));   
+    }
+}
